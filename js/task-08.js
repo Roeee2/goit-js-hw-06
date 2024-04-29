062e7e99a50261eca0a51c95db8d298ae37e5908
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-04-29 18:11:50 +0100

    task8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = this.elements.email;
+  const passwordInput = this.elements.password;
+
+  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
+    alert("All fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+
+  this.reset();
+});
