71043323acd02d07fad9dde43fb93e42ae5ee9ef
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-04-29 15:36:32 +0100

    task1-done

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesList = document.getElementById("categories");
+const categoryItems = categoriesList.querySelectorAll(".item");
+console.log("Number of categories:", categoryItems.length);
+categoryItems.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryElements = category.querySelectorAll("li").length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryElements}`);
+});
