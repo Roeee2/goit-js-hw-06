2dffed007861b8fb765d4a4f08b68c752ae62c0b
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-04-30 10:46:53 +0100

    task7-corrected

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,7 @@
 const fontSizeControl = document.getElementById("font-size-control");
 const textSpan = document.getElementById("text");
+const initialFontSize = fontSizeControl.value;
+textSpan.style.fontSize = initialFontSize + "px";
 fontSizeControl.addEventListener("input", function () {
   textSpan.style.fontSize = this.value + "px";
 });
